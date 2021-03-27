117441292441edf0bff6134fbe85ef53499c53b7
Aditya Chandra <61273049+Aditya-Chandra@users.noreply.github.com>
2021-03-27 08:50:17 +0530

    Update settings.py

--- textutils_proj/settings.py
+++ textutils_proj/settings.py
@@ -127,4 +127,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+heroku config:set DISABLE_COLLECTSTATIC=1
